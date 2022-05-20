ea1479913b53ed695355d146349dffa2d2976115
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-20 14:46:59 -0700

    try again

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,4 @@
 // IMPORT MODULES under test here:
-import { renderList } from '../render-utils.js';
 // import { example } from '../example.js';
 
 const test = QUnit.test;
@@ -7,13 +6,13 @@ const test = QUnit.test;
 test('testing ul render', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<ul><li>4: apples</li></ul>`;
+    const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderList({ qty: 4, name: 'apples' });
+    const actual = true;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected);
+    expect.equal(actual, expected);
 });
